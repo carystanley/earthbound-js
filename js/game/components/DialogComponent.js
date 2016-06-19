3cbd5e51e144d9bc90efa8ad748ed4144769e578
Cary Stanley <carystanley@yahoo.com>
2016-06-18 20:17:36 -0700

    Update DialogComponent to handle char-by-char

--- js/game/components/DialogComponent.js
+++ js/game/components/DialogComponent.js
@@ -5,25 +5,48 @@ DialogComponent = function(game) {
     this.container.targetWidth = 152;
     this.container.targetHeight = 64
     this.container.fixedToCamera = true;
-    this.chatTxt = game.make.bitmapText(8, 8, 'basic', '', 16, this.container);
-    this.chatTxt.maxWidth = 144;
-    this.container.addChild(this.chatTxt);
+    this.chatTxt = [];
+    this.lineCount = 3;
+    for (var i = 0; i < this.lineCount; i++) {
+        var chatLine = game.make.bitmapText(8, 8 + (i * 16), 'basic', '', 16, this.container);
+        chatLine.maxWidth = 128;
+        this.container.addChild(chatLine);
+        this.chatTxt.push(chatLine);
+    }
     this.container.visible = false;
-    this.buffer = '';
-    this.displayText = '';
+    this.clear();
     game.time.events.loop(25, this.update, this);
 };
 
 DialogComponent.prototype.update = function() {
 	if (this.buffer.length > 0) {
-	    this.displayText += this.buffer.substr(0, 1);
-	    this.buffer = this.buffer.substr(1);
-	    this.chatTxt.setText(this.displayText);
-	}
+		// If we have more text to show, and we have reached lineCount
+	    if (this.currentLine >= this.lineCount) {
+	        this.currentLine--;
+		    for (var i = 0; i < (this.lineCount - 1); i++) {
+			    this.chatTxt[i].setText(this.chatTxt[i+1].text);
+			}
+		    this.chatTxt[this.currentLine].setText('');
+		}
+
+        if (this.buffer[0].length > 0) {
+	        var c = this.buffer[0].substr(0, 1);
+	        this.buffer[0] = this.buffer[0].substr(1);
+	        this.chatTxt[this.currentLine].setText(this.chatTxt[this.currentLine].text + c);
+	    } else {
+	        this.buffer.shift();
+	        this.currentLine++;
+	    }
+    }
 };
 
-DialogComponent.prototype.setText = function(displayText) {
-	this.buffer = displayText;
+DialogComponent.prototype.setText = function(text) {
+    do {
+        // Private method I wish was Public
+        var line = this.chatTxt[0].scanLine(this.chatTxt[0]._data.font, 1, text);
+        this.buffer.push(line.text);
+        text = text.substr(line.text.length + 1);
+    } while (line.end === false);
 };
 
 DialogComponent.prototype.show = function() {
@@ -36,9 +59,11 @@ DialogComponent.prototype.hide = function() {
 };
 
 DialogComponent.prototype.clear = function() {
-    this.buffer = '';
-    this.displayText = '';
-    this.chatTxt.setText(this.displayText);
+    this.buffer = [];
+    this.currentLine = 0;
+    for (var i = 0; i < this.lineCount; i++) {
+        this.chatTxt[i].setText('');
+    }
 };
 
 module.exports = DialogComponent;

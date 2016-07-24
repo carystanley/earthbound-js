167f8a747259bc2a072fad8d2034b33de6fce3da
Cary Stanley <carystanley@yahoo.com>
2016-07-23 20:06:00 -0700

    Initial Inventory

---
+++ js/game/Inventory.js
@@ -0,0 +1,29 @@
+
+var Inventory = function (maxSize) {
+    this.items = [];
+    this.maxSize = maxSize;
+};
+
+Inventory.prototype.add = function(itemId) {
+   if (this.items.length < maxSize) {
+       this.items.push(itemId);
+   }
+}
+
+Inventory.prototype.remove = function(itemPos) {
+    this.items.splice(itemPos, 1);
+}
+
+Inventory.prototype.getList = function() {
+    return this.items;
+}
+
+Inventory.prototype.contains = function(itemId) {
+    this.items.indexOf(itemId);
+}
+
+Inventory.prototype.isFull = function() {
+    return (this.items.length >= this.maxSize);
+}
+
+module.exports = Inventory;

8c9ccb6ffae6264a104446e0c24b05901369ce91
carystanley <carystanley@yahoo.com>
2017-01-25 18:34:42 -0800

    Show / Hide Battle Background

--- js/substates/Battle.js
+++ js/substates/Battle.js
@@ -7,6 +7,7 @@ BattleSubState.prototype.transitionIn = function(callback) {
     var state = this.parent;
     state.encounterIn(function () {
         state.hideWorld();
+        state.showBattle();
         callback();
     });
 }
@@ -21,6 +22,7 @@ BattleSubState.prototype.update = function() {
 BattleSubState.prototype.transitionOut = function(callback) {
     var state = this.parent;
     state.showWorld();
+    state.hideBattle();
     state.encounterOut(callback);
 }
 
